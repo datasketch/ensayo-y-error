b6ab880833cedcd77a45d93b721d495ffa08de47
David Daza <03dazal@gmail.com>
2019-11-25 17:04:38 -0500

    homepage no responsive

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        'oswald': ['Oswald', 'sans-serif'],
+        'open-sans': ['"Open Sans"', 'sans-serif'],
+      },
+      colors: {
+        'spring-green': '#21f4a7',
+        'outer-space': '#213133'
+      },
+      spacing: {
+        '72': '18rem',
+        '84': '21rem',
+        '96': '24rem',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
